a49ba1a522276c1154a741d12764c2fb26ae8af6
Louie Fitzpatrick <fitz4492@gmail.com>
2022-02-07 22:48:40 -0600

    overwriting default express error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,14 +2,23 @@
 // @route   GET /api/goals
 // @access  Private
 const getAllGoals = (req, res) => {
-    res.status(200).json({ message: 'Get all goals' });
+    if (req.body.text) {
+        res.status(200).json({ message: 'Get all goals' });
+    } else {
+        res.status(400).json({ message: 'Please add a text field.' });
+    }
 };
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
 const setGoal = (req, res) => {
-    res.status(200).json({ message: 'Create a Goal' });
+    if (req.body.text) {
+        res.status(200).json({ message: 'Create a Goal' });
+    } else {
+        res.status(400);
+        throw new Error("Please add a text field");
+    }
 };
 
 // @desc    Update goal
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const port = process.env.PORT || 5000;
-
-
-
+const { errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/goals', require('./routes/goals'));
+
+// Overwrites default express error handling
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Started server on port ${port}`));
