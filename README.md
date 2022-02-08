441b13dc55e0acac3e85fcf1eed1ba1b4236c032
Louie Fitzpatrick <fitz4492@gmail.com>
2022-02-07 22:09:22 -0600

    dotenv stuff

---
+++ README.md
@@ -0,0 +1,4 @@
+# MERN App
+
+### Troubleshooting
+    - Whenever you make a change the *.env* file, you must manually restart the server.
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log("helloworld");
+const express = requre("express");
+const dotenv = require("dotenv");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Started server on port ${T}`);)
